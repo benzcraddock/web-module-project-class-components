cef4baa2c8be53a4b78b16ea7dd9edeab669b521
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2021-11-29 20:52:07 -0500

    add button added

--- src/App.js
+++ src/App.js
@@ -37,6 +37,24 @@ class App extends React.Component {
 
   // this component is going to take care of state, and any change handlers you need to work with your state
 
+  handleAdd = () => {
+    // 1. setState
+    // 2. change todos
+    // 3. make a copy of todos
+    // 4. add a new todo to the end of our todo list
+
+    const newTodo = {
+      task: 'Cook things',
+      id: Date.now(),
+      completed: false
+    }
+
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo]
+    })
+  }
+
   handleClear = () => {
     // 1. setState
     // 2. loop through all todos
@@ -58,7 +76,7 @@ class App extends React.Component {
       <div>
         <h2>Welcome to your Todo App!</h2>
         <TodoList todos={todos}/>
-        <TodoForm />
+        <TodoForm handleAdd={this.handleAdd} />
         <button onClick={this.handleClear}>Clear</button>
       </div>
     );
